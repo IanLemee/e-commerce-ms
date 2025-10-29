a9a074148a2c7768f2c9a422f14e0e52773f7d68
IanLemee <ianlemee@hotmail>
2025-10-29 16:47:23 -0300

    feat: creating attribute uuid

--- user/src/main/java/com/tech/ian/user/model/UserEntity.java
+++ user/src/main/java/com/tech/ian/user/model/UserEntity.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users_tb")
@@ -22,6 +23,7 @@ public class UserEntity implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private UUID uuid;
     private String name;
     private String email;
     private String password;
