1a92f940542ac6fe1d5a0a928938e41c898697a9
IanLemee <ianlemee@hotmail>
2025-10-29 12:10:49 -0300

    feat: creating query to find user by email

--- user/src/main/java/com/tech/ian/user/repo/UserRepository.java
+++ user/src/main/java/com/tech/ian/user/repo/UserRepository.java
@@ -2,7 +2,14 @@ package com.tech.ian.user.repo;
 
 import com.tech.ian.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    @Query(value = "SELECT * FROM users_tb WHERE EMAIL = :email", nativeQuery = true)
+    Optional<UserEntity> loadUserByUsername(@Param("email") String email);
 }
