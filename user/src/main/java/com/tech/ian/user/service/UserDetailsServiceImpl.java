3741cd5843b4dd177e3e981586fe30364d337a65
IanLemee <ianlemee@hotmail>
2025-10-29 12:13:34 -0300

    feat: implementing method loadUserByUsername on UserDetailServiceImpl

--- user/src/main/java/com/tech/ian/user/service/UserDetailsServiceImpl.java
+++ user/src/main/java/com/tech/ian/user/service/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.tech.ian.user.service;
 
+import com.tech.ian.user.repo.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.loadUserByUsername(email).orElseThrow(() -> new UsernameNotFoundException("Email not found"));
     }
 }
