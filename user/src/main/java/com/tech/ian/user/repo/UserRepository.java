f0b82cc563c7282946d3e2c1c35ace3c31a76864
IanLemee <ianlemee@hotmail>
2025-10-31 22:04:32 -0300

    fix: fixing the query to search for a especific user by his email and change the method name

--- user/src/main/java/com/tech/ian/user/repo/UserRepository.java
+++ user/src/main/java/com/tech/ian/user/repo/UserRepository.java
@@ -4,12 +4,11 @@ import com.tech.ian.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    @Query(value = "SELECT * FROM users_tb WHERE EMAIL = :email", nativeQuery = true)
-    Optional<UserEntity> loadUserByUsername(@Param("email") String email);
+    @Query(value = "SELECT * FROM users_tb WHERE user_email = :email", nativeQuery = true)
+    Optional<UserEntity> findUserByEmail(@Param("email") String email);
 }
