e0908d5d65e12db115a4133e9ff7845d553bcdbf
IanLemee <ianlemee@hotmail>
2025-10-31 22:05:32 -0300

    feat: adding annotation to especify that this class is a module

--- user/src/main/java/com/tech/ian/user/security/AuthFilter.java
+++ user/src/main/java/com/tech/ian/user/security/AuthFilter.java
@@ -6,16 +6,16 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Configuration
+@Component
 public class AuthFilter extends OncePerRequestFilter {
 
     private final JwtUtils jwtUtils;
