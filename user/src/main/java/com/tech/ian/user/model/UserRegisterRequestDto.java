fcdd319aa36723d6cd0e04132407dc09aab8693e
IanLemee <ianlemee@hotmail>
2025-10-31 22:02:02 -0300

    refactor: removing notNull annotation from attributes

--- user/src/main/java/com/tech/ian/user/model/UserRegisterRequestDto.java
+++ user/src/main/java/com/tech/ian/user/model/UserRegisterRequestDto.java
@@ -6,16 +6,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserRegisterRequestDto(
-        @NotNull @NotBlank
-        @Size(min = 2, max = 64)
+        @NotBlank @Size(min = 2, max = 64)
         String name,
-        @NotNull @NotBlank
-        @Email
+        @NotNull @Email
         String email,
-        @NotNull @NotBlank
-        @Size(min = 11, max = 30)
+        @NotBlank @Size(min = 11, max = 30)
         String password,
-        String profilePicture,
-        Role role
+        String profilePicture
 ) {
 }
