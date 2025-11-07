79f4e86b777ca0dc4a3951447fa92360ac073bb2
IanLemee <ianlemee@hotmail>
2025-11-07 20:56:04 -0300

    feat: create s3 client and s3 properties

---
+++ user/src/main/java/com/tech/ian/user/config/aws/S3Properties.java
@@ -0,0 +1,16 @@
+package com.tech.ian.user.config.aws;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "aws.s3")
+@Getter
+@Setter
+public class S3Properties {
+
+    private String bucketName;
+    private String region;
+}
