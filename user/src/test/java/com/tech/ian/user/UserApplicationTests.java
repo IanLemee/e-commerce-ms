7e3dcf5708134a9d5ed43e4c04a008651f0916da
IanLemee <ianlemee@hotmail>
2025-12-10 12:36:21 -0300

    fix: disable database for unit tests

--- user/src/test/java/com/tech/ian/user/UserApplicationTests.java
+++ user/src/test/java/com/tech/ian/user/UserApplicationTests.java
@@ -1,14 +1,26 @@
 package com.tech.ian.user;
 
+import com.tech.ian.user.repo.UserRepository;
+import com.tech.ian.user.repo.VerificationFailureRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class UserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockitoBean
+    private UserRepository userRepository;
+
+    @MockitoBean
+    private VerificationFailureRepository verificationFailureRepository;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
