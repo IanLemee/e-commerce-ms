6e9c78d78bcd23d14a4df273de097375822fa960
IanLemee <ianlemee@hotmail.com>
2026-01-02 17:00:25 -0300

    .

--- notification/src/test/java/com/tech/ian/notification/NotificationApplicationTests.java
+++ notification/src/test/java/com/tech/ian/notification/NotificationApplicationTests.java
@@ -5,7 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotificationApplicationTests {
-
+	// TODO
+	//  CREATE TESTS
 	@Test
 	void contextLoads() {
 	}
